1c28b123e084bbc59ffd5f658661ae8a8e55b3b7
Wulver2 <ross.shayla21@gmail.com>
2025-09-10 11:06:49 -0400

    added a display function to createGameboard to display what is in the gameBoard array on the webpage

--- ttt.js
+++ ttt.js
@@ -1,17 +1,26 @@
 function createGameboard() {
     // need to account for a player attempting to add a X/O
     // to a spot that already has one
-    // turn this into a private variable later and add a function to
-    // display the board instead
     let gameBoard = [["*","*","*"], ["*","*","*"], ["*","*","*"]];
     const addX = function(row, col) { gameBoard[row][col] = "X"};
     const addO = function(row, col) { gameBoard[row][col] = "O"};
-    return {gameBoard, addX, addO};
+    const display = function() {
+        let container = document.getElementById("container");
+        for(let r = 0; r < gameBoard.length; r++) {
+            // may change element type
+            let row = document.createElement("p");
+            for(let c = 0; c < gameBoard[r].length; c++) {
+                row.textContent += gameBoard[r][c];
+            }
+            container.appendChild(row);
+        }
+    };
+    // may add function to play/update display when addX or add0 is used
+    
+    return {addX, addO, display};
 }
 
 var game = createGameboard();
-console.log(game.gameBoard);
+game.display();
 game.addX(0,0);
-console.log(game.gameBoard);
 game.addO(2,1);
-console.log(game.gameBoard);
