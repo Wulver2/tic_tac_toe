c118cfda48e9f7b08f112fb4a003c5755f0a5d6c
Wulver2 <ross.shayla21@gmail.com>
2025-09-15 10:13:06 -0400

    Added the outline for the gameplay loop

--- ttt.js
+++ ttt.js
@@ -5,6 +5,7 @@ function createGameboard() {
     const addX = function(row, col) { gameBoard[row][col] = "X"};
     const addO = function(row, col) { gameBoard[row][col] = "O"};
     const display = function() {
+        //add event for buttons so that they add the players mark
         let container = document.getElementById("game");
         for(let r = 0; r < gameBoard.length; r++) {
             // may change element type
@@ -28,16 +29,17 @@ function createGameboard() {
     return {addX, addO, display};
 }
 
-function players() {
+function players(mark) {
     var score = 0;
     //X or O
     const mark = "";
-    const display = function() {
+    const displayScore = function() {
         let container = document.getElementById("game");
         let s = document.createElement("h3");
         s.textContent = score;
         container.appendChild(s);
     };
+    // change mark for new game
     const changeMark = function(newMark) {
         mark = newMark;
     };
@@ -45,10 +47,28 @@ function players() {
 }
 
 function game() {
-    const playerOne = players();
-    const playerTwo = players();
+    // for now player 1 will be fixed to only add X's
+    // and player 2 will only be able to add O's.
+    // later on want the players to be able to choose their own
+    const playerOne = players("X");
+    const playerTwo = players("O");
+    var board = createGameboard();
+
+    const turn = function(player) {
+        // displays "X's turn" or "O's turn"
+
+    };
+    const gameplay = function() {
+        // X goes frist, then O. 
+        // prevent players from marking same tile
+        // check for 3 in a row.
+        // repeat until 3 in a row occurs or
+        // board is filled
+    };
+    return {board}
+
 }
-var board = createGameboard();
-board.display();
-board.addX(0,0);
-board.addO(2,1);
+var play = game();
+play.board.display();
+play.board.addX(0,0);
+play.board.addO(2,1);
